e13d66c9150ae167ba41754f26ffe5be1dc384f0
klballard <kyleleeballard@gmail.com>
2020-09-29 20:35:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//Bootstrapped Navbar code
+window.onscroll = function() {navScroll()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function navScroll() {
+  if (window.pageYOffset >= sticky) {
+navbar.classList.add("sticky")
+  } else {
+navbar.classList.remove("sticky");
+}
+}
+
+var $navBar = $('.navbar');
+
+var navPos = $navBar.offset().top;
+
+$(window).scroll(function() {
+
+    var scrollPos = $(this).scrollTop();
+
+    if (scrollPos >= navPos) {
+        $navBar.addClass('fixed');
+    } else {
+        $navBar.removeClass('fixed');
+    }
+
+});
+
